d7bbe0f60fef0e7816905c2bc203cb947abac189
Raval Sahil <ravalsahil1311@gmail.com>
2025-08-03 20:33:24 +0530

    prediction pipeline and hypterparameter tuning completed

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from exception import CustomException
 
@@ -22,12 +23,16 @@ def save_object(
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -40,5 +45,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
